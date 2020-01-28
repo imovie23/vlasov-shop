806981c1c4bb7e803fe3dd84699be84d4482b664
Misha Starodubets <starodubetsm@gmail.com>
2020-01-28 17:00:49 +0200

    Typescript Travis CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
     ],
     plugins: ["@typescript-eslint", "prettier"],
     parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
     env: {
         browser: true,
--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `VlasovShop`,
+        short_name: `Vlasov`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `vlasov-shop-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        crossOrigin: `use-credentials`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
